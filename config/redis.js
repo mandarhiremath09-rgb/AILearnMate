a128f06956236f6b965eec13a25cbb2c6fae894c
Mandar Mahadev Hiremath <mandarhiremath.09@gmail.com>
2026-05-21 09:27:38 +0530

    Add Redis connection

---
+++ config/redis.js
@@ -0,0 +1,17 @@
+const redis = require('redis');
+require('dotenv').config();
+
+const client = redis.createClient({
+  host: process.env.REDIS_HOST || 'localhost',
+  port: process.env.REDIS_PORT || 6379,
+});
+
+client.on('error', (err) => {
+  console.log('Redis Client Error', err);
+});
+
+client.on('connect', () => {
+  console.log('Connected to Redis');
+});
+
+module.exports = client;
